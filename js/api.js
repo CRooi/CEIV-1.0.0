7298295fdb99c5354cab05b800f13dc53b9c3296
CRooi <107915149+CRooi@users.noreply.github.com>
2022-06-21 16:44:27 +0800

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,20 @@
+//地震速报
+var infoApi = "https://**.**/**"; //填入地震速报api链接
+var infoInstitute = "response.institute"; //填入地震速报api中 发布机构 的json路径  这里已举例出
+var infoOccurTime = "data.occurTime"; //填入地震速报api中 地震发生时间 的json路径  这里已举例出
+var infoEpicenter = "data.epicenter"; //填入地震速报api中 震中 的json路径  这里已举例出
+var infoMagnitude = "data.magnitude"; //填入地震速报api中 地震规模 的json路径  这里已举例出
+var infoDepth = "data.depth"; //填入地震速报api中 震源深度 的json路径  这里已举例出
+var infoMaxInt = "data.maxInt"; //填入地震速报api中 最大烈度 的json路径  这里已举例出
+var infoMd5 = "data.md5"; //填入地震速报api中 md5 的json路径  这里已举例出
+
+//地震预警
+var eewApi = "https://**.**/**"; //填入地震预警api链接
+var eewInstitute = "response.institute"; //填入地震预警api中 发布机构 的json路径  这里已举例出
+var eewOccurTime = "data.occurTime"; //填入地震预警api中 地震发生时间 的json路径  这里已举例出
+var eewOccurTimestamp = "data.occurTimestamp"; //填入地震预警api中 地震发生时间戳 的json路径  这里已举例出
+var eewEpicenter = "data.epicenter"; //填入地震预警api中 震中 的json路径  这里已举例出
+var eewMagnitude = "data.magnitude"; //填入地震预警api中 地震规模 的json路径  这里已举例出
+var eewDepth = "data.depth"; //填入地震预警api中 震源深度 的json路径  这里已举例出
+var eewMaxInt = "data.maxInt"; //填入地震预警api中 最大烈度 的json路径  这里已举例出
+var eewMd5 = "data.md5"; //填入地震预警api中 md5 的json路径  这里已举例出
